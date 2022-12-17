85c8c2c66ea0af558984824b3622ca1a358a5904
valentinbiz <valentin.bizgan@outlook.com>
2022-12-17 18:59:29 +0000

    Finish task 18 with testing suite

--- models/models.comments.js
+++ models/models.comments.js
@@ -34,6 +34,33 @@ const insertComment = (articleId, body, username) => {
   });
 };
 
+const updateCommentVotes = (commentId, updateInfo) => {
+  const updateCommentsVotesQueryString = `
+  UPDATE comments 
+  SET votes = $1 
+  WHERE comment_id = $2 
+  RETURNING *;
+  `;
+
+  const getCurrentVotesQueryString = `
+  SELECT votes 
+  FROM comments 
+  WHERE comment_id = $1
+  `;
+
+  return db
+    .query(getCurrentVotesQueryString, [commentId])
+    .then(({ rows }) => {
+      if (rows.length === 0)
+        return Promise.reject({ status: 404, msg: "Not found!" });
+      return rows[0];
+    })
+    .then(({ votes }) => {
+      const newVotes = votes + updateInfo;
+      return db.query(updateCommentsVotesQueryString, [newVotes, commentId]);
+    });
+};
+
 const deleteCommentById = (commentId) => {
   const queryString = `
   DELETE FROM comments
@@ -48,5 +75,6 @@ const deleteCommentById = (commentId) => {
 module.exports = {
   selectCommentsByArticleId,
   insertComment,
+  updateCommentVotes,
   deleteCommentById,
 };
