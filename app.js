47928543a1fcc5c261ee8dd5f42608817778aeed
valentinbiz <valentin.bizgan@outlook.com>
2023-01-09 11:20:14 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const {
   error404Handler,
   error400Handler,
@@ -9,6 +11,7 @@ const {
 const apiRouter = require("./routes/api.router");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
